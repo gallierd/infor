d172e06acfdacb90c3b9716fc3adee784c16faea
Adnan Tri Wibowo Mukti <82567453+gallierd@users.noreply.github.com>
2024-08-02 21:39:36 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+// Ketika pengguna menggulir ke bawah 20px dari bagian atas dokumen, tampilkan tombolnya
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("back-to-top-btn").style.display = "block";
+  } else {
+    document.getElementById("back-to-top-btn").style.display = "none";
+  }
+  
